97f7d0f617c8de77bb1eebd45d625aaf8f64c343
dr3nan <dolgarrenan@gmail.com>
2023-03-31 11:05:13 +0200

    refactor: all values are now of type number, there is no need to change types so often impacting performance and the code looks cleaner, refs #101

--- calculator/src/app/components/calculator/Calculator.tsx
+++ calculator/src/app/components/calculator/Calculator.tsx
@@ -4,34 +4,37 @@ import './Calculator.scss';
 
 const Calculator: React.FC = () => {
   const [result, setResult] = useState<number>(0);
-  const [numberInput, setNumberInput] = useState<string>('');
+  const [numberInput, setNumberInput] = useState<number>(0);
 
-  const handleNumberInput = (value: string) => {
-    setNumberInput(prevInput => prevInput + value);
+  const handleNumberInput = (value: number) => {
+    if (numberInput.toString().length > 9) {
+      alert('The number is too long');
+    } else {
+      setNumberInput(prevInput => prevInput * 10 + value);
+    }
   };
 
   const handleOperator = (operator: Operator) => {
     if (!numberInput) {
       alert('Please enter a number');
     } else {
-      const number = +numberInput;
       switch (operator) {
         case '+':
-          setResult(prevResult => prevResult + number);
+          setResult(prevResult => prevResult + numberInput);
           break;
         case '-':
-          setResult(prevResult => prevResult - number);
+          setResult(prevResult => prevResult - numberInput);
           break;
         case '*':
-          setResult(prevResult => prevResult * number);
+          setResult(prevResult => prevResult * numberInput);
           break;
         case '/':
-          setResult(prevResult => prevResult / number);
+          setResult(prevResult => prevResult / numberInput);
           break;
         default:
           break;
       }
-      setNumberInput('');
+      setNumberInput(0);
     }
   };
 
@@ -39,20 +42,23 @@ const Calculator: React.FC = () => {
     if (!numberInput) {
       alert('Please enter a number');
     } else {
-      const number = +numberInput;
-      setResult(prevResult => prevResult + number);
-      setNumberInput('');
+      setResult(prevResult => prevResult + numberInput);
+      setNumberInput(0);
     }
   };
 
   const handleClear = () => {
     setResult(0);
-    setNumberInput('');
+    setNumberInput(0);
   };
 
   const handleDelete = () => {
-    setNumberInput(prevInput => prevInput.slice(0, -1));
+    setNumberInput(prevInput => Math.floor(prevInput / 10));
   };
+
+  // here we could add another event handler for the keyboard input
+  // and use the event.key to determine which button was pressed
+  // and then call the appropriate function
 
   return (
     <div className='calculator'>
@@ -62,7 +68,7 @@ const Calculator: React.FC = () => {
         <input
           type='text'
           value={numberInput}
-          onChange={(event) => setNumberInput(event.target.value)}
+          onChange={(event) => setNumberInput(Number(event.target.value))}
           onKeyDown={(event) => {
             if (event.key === 'Enter') {
               handleEqual();
@@ -71,16 +77,16 @@ const Calculator: React.FC = () => {
         />
       </div>
       <div className='number-buttons'>
-        <button onClick={() => handleNumberInput('1')}>1</button>
-        <button onClick={() => handleNumberInput('2')}>2</button>
-        <button onClick={() => handleNumberInput('3')}>3</button>
-        <button onClick={() => handleNumberInput('4')}>4</button>
-        <button onClick={() => handleNumberInput('5')}>5</button>
-        <button onClick={() => handleNumberInput('6')}>6</button>
-        <button onClick={() => handleNumberInput('7')}>7</button>
-        <button onClick={() => handleNumberInput('8')}>8</button>
-        <button onClick={() => handleNumberInput('9')}>9</button>
-        <button onClick={() => handleNumberInput('0')}>0</button>
+        <button onClick={() => handleNumberInput(1)}>1</button>
+        <button onClick={() => handleNumberInput(2)}>2</button>
+        <button onClick={() => handleNumberInput(3)}>3</button>
+        <button onClick={() => handleNumberInput(4)}>4</button>
+        <button onClick={() => handleNumberInput(5)}>5</button>
+        <button onClick={() => handleNumberInput(6)}>6</button>
+        <button onClick={() => handleNumberInput(7)}>7</button>
+        <button onClick={() => handleNumberInput(8)}>8</button>
+        <button onClick={() => handleNumberInput(9)}>9</button>
+        <button onClick={() => handleNumberInput(0)}>0</button>
       </div>
       <div className='operator-buttons'>
         <button onClick={() => handleOperator('+')}>+</button>
